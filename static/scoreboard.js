205d3d32ce56ebff58bf1955b0ad229307dbfe6d
Josh Choi <jc4881@columbia.edu>
2020-04-13 21:23:01 -0400

    Finished

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -8,7 +8,7 @@ function display_scoreboard(scoreboard){
 function addTeamView(id, name, score){
   var team_template = $("<div class = row></div>");
   var name_template = $("<div class = col-md-5></div>");
-  var score_template = $("<div class = col-md-2></div>");
+  var score_template = $("<div id=" + id + " class = col-md-2></div>");
   var button_template = $("<div class = col-md-2></div>");
   var increase_button = $("<button class = increase-button>+</button>");
   $(increase_button).click(function(){
@@ -32,7 +32,8 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+        var score = result.scoreboard[id - 1].score;
+        $("#" + id).html(score);
     },
     error: function(request, status, error){
         console.log("Error");
