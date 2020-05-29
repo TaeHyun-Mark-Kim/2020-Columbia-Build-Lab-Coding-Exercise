fd6589d6ca340b648c0859024d09d3cd57adde57
Rojeen Farkhoor <rojeen.farkhoor@gmail.com>
2020-05-28 17:29:14 -0700

    sor the teams whenever there is a score change in server.py

--- server.py
+++ server.py
@@ -38,19 +38,44 @@ scoreboard = [
 
 @app.route('/')
 def show_scoreboard():
+    # render template using name of template and the variables you want to pass to the template engine as keyword arguments
+    # template = scoreboard.html
+    # argument for template engine = scoreboard (list of dictionaries with each team's ID, name, and score)
+
     return render_template('scoreboard.html', scoreboard = scoreboard) 
 
 @app.route('/increase_score', methods=['GET', 'POST'])
 def increase_score():
     global scoreboard
 
+    # parse request as JSON
     json_data = request.get_json()   
-    team_id = json_data["id"]  
     
-    for team in scoreboard:
-        if team["id"] == team_id:
-            team["score"] += 1
+    # determine ID of the team to increase score
+    team_id = json_data["id"]  
 
+    prev_score = 0
+    i = 0
+
+    while True:
+        new_score = 0
+        
+        if scoreboard[i]["id"] == team_id:
+            scoreboard[i]["score"] += 1
+            new_score = scoreboard[i]["score"]
+        
+            if ((i>0) and (new_score > prev_score)):
+                # swap the positions of the two teams in the list
+                temp_team = scoreboard[i].copy()
+                scoreboard[i] = scoreboard[i-1].copy()
+                scoreboard[i-1] = temp_team.copy()
+
+            break
+        else:
+            prev_score = scoreboard[i]["score"]
+            i+=1
+
+    # return the scoreboard as JSON
     return jsonify(scoreboard=scoreboard)
 
 
